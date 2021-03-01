06d2d803df4f0963bf3804b040f6384bec4cfe7f
Alex Plotnikow <plotnikovalex18@yandex.ru>
2021-03-01 15:27:45 +0300

    change HW after revision

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,18 +3,15 @@ function getResult(a,b,c){
     // код для задачи №1 писать 
     let d = b**2 - 4*a*c;
     let x = [];
-    if (d < 0) {
-      return x;
-    } else if (d === 0) {
+    if (d === 0) {
       let x1 = (-b + Math.sqrt(d))/2*a;
-      x.push(x1);
-      return x;
+      x.push(x1);     
     } else if (d > 0) {
       let x1 = (-b + Math.sqrt(d))/2*a;
       let x2 = (-b - Math.sqrt(d))/2*a;
       x.push(x1,x2);
-      return x;
     }
+    return x;
 }
 
 function getAverageMark(marks){
@@ -24,35 +21,24 @@ function getAverageMark(marks){
     let sum = 0;
     if (arr.length === 0) {
       averageMark = 0;
-    } else if (arr.length > 0 && arr.length < 6) {
-      for (let i = 0; i < arr.length; i++){
-        sum += arr[i];
-      }
-      averageMark = sum/arr.length;
     } else {
       arr = arr.slice(0, 5);
       for (let i = 0; i < arr.length; i++){
         sum += arr[i];
       }
       averageMark = sum/arr.length;
-
-    }
+	}
     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    let guestName = name;
-    let guestBirthday = dateOfBirthday;
-    let today = new Date();
-    let year = today.getFullYear();
-    let guestYear = guestBirthday.getFullYear();
-    let yearDiff = year - guestYear;
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
     let result = '';
-    if (yearDiff < 18) {
-      result = `Сожалею, ${guestName}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+    if (age < 18) {
+      result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
     } else {
-      result = `Не желаете ли олд-фэшн, ${guestName}?`
+      result = `Не желаете ли олд-фэшн, ${name}?`
     }
     return result;
     // return result;
