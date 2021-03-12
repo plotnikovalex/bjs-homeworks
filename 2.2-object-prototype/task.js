321b302143b48d1d6823c99ef0d798a90987d99a
Alex Plotnikow <plotnikovalex18@yandex.ru>
2021-03-12 13:20:25 +0300

    HW 2.2 without first task

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,7 +1,36 @@
-//String.prototype.isPalindrome - для задачи №1
+// const String = function(str) {
+// 	this.str = str.toLowerCase();
+// }
+// const str = new String (str);
+// String.prototype.isPalindrome = function(str){
+// 	let valid = true;
+// 	for (let i = 0, j = str.length - 1; i < str.length; i++, j--) {
+// 		if (str[i] !== str[j]) {
+// 			valid = false; 
+// 			break;
+// 		}
+// 	}
+// 	return valid;
+// }
+
+// str.prototype = Object.create(String.prototype);
+// String.isPalindrome();
+// console.log(string.str);
+
+
+
+	
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
+    let sum = 0;
+    let average;
+      for (let i = 0; i < marks.length; i++) {
+	  	sum += marks[i];
+	  }
+    average = sum/marks.length;
+   let roundedAverage = Math.round(average);
+   return roundedAverage;
     // return averageMark
 }
 
