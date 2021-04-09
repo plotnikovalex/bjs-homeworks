a2bc9b08ab97ab17d2a8c5793a14d53344c7fa84
Alex Plotnikow <plotnikovalex18@yandex.ru>
2021-04-09 10:51:17 +0300

    2.3 right comm

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -91,7 +91,7 @@ class Library {
   giveBookByName(bookName) {
     for (let i = 0; i < this.books.length; i++) {
       if (this.books[i].name === bookName) {
-        return this.books.splice(i,1);
+        return this.books.splice(i,1)[0];
       } 
     }
     return null;
